58c789c6d62e41d7d1cddcd0dd8919458d6ded6a
kfatyuip <kfatyuip@gmail.com>
2024-06-08 21:45:59 +0800

    update route.rs

--- src/route.rs
+++ src/route.rs
@@ -23,3 +23,16 @@ pub fn location_index(location: &str, f: Vec<String>) -> String {
 
     return html.clone();
 }
+
+#[inline(always)]
+pub fn extension_match(extension: &str) -> String {
+    match extension {
+        "jpg" | "png" | "jpeg" | "gif" => format!("image/{extension}"),
+        "mp3" | "ogg" | "wav" | "mp4" => format!("audio/{extension}"),
+        "txt" | "text" | "toml" | "yaml" | "yml" | "ini" | "xml" | "csv" | "md" | "json" => {
+            "text/plain".to_owned()
+        }
+        "html" | "htm" => "text/html".to_owned(),
+        &_ => "application/octet-stream".to_owned(),
+    }
+}
